df8892527b808862a8bcd1af46728f779ea94eed
Changer He <changerheee@gmail.com>
2021-04-15 14:20:33 +0800

    fix: revert useful random function

--- utils/random.js
+++ utils/random.js
@@ -1,8 +1,13 @@
+const { TOTAL_AREA, BACK_SEAT_NUM, TOTAL_ROW, SINGLE_MAX_TICKET_MUM } = require("../constants");
+
 // 获取随机数, 左闭右闭区间
 const random = (start, end) => {
   return start + Math.floor(Math.random() * (end - start + 1));
 };
 
+const randomPeople = () => random(1, SINGLE_MAX_TICKET_MUM);
+
 module.exports = {
   random,
+  randomPeople,
 };
